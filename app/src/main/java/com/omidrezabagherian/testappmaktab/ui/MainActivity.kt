de2045e4c64256b91b6501afce6ee08f1ae47329
OmidrezaBagherian <OmidCoder21@gmail.com>
2022-04-21 02:52:40 -0700

    Start project and add packages in project and move MainActivity in ui

---
+++ app/src/main/java/com/omidrezabagherian/testappmaktab/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.omidrezabagherian.testappmaktab.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.omidrezabagherian.testappmaktab.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
